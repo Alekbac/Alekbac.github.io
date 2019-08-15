dcc04db0c7f73f2c51bfb7f3aacd0cb1474a807f
infinite-dawn-60500 <aleksandra.bachanek1@gmail.com>
2019-08-15 20:21:14 +0200

    scrpt.js, poprawka

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://afternoon-retreat-58512.herokuapp.com/task/';
+  const apiRoot = 'https://afternoon-retreat-58512.herokuapp.com/v1/task/';
   const trelloApiRoot = 'https://afternoon-retreat-58512.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
